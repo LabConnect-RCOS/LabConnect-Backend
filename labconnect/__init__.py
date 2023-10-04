dfae376103c9367884f5a5fc7dc60ec317fea236
Rafael Cenzano <savagecoder77@gmail.com>
2023-10-04 00:04:07 -0400

    Restructure initialization file

--- labconnect/__init__.py
+++ labconnect/__init__.py
@@ -1,17 +1,54 @@
 import os
+import logging
 
 # Import Flask modules
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-environment = os.environ.get("CONFIG", "config.TestingConfig")
+# from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
-# Create flask app object
-app = Flask(__name__)
-app.config.from_object(environment)
+
+csrf_protection = CSRFProtect()
 
 # Create Database object
 # db = SQLAlchemy(app)
 
-# Import all views
-import labconnect.views
+
+def create_app() -> Flask:
+    # Create flask app object
+    app = Flask(__name__)
+    app.config.from_object(os.environ.get("CONFIG", "config.TestingConfig"))
+
+    initialize_extensions(app)
+    register_blueprints(app)
+
+    return app
+
+
+# ----------------
+# Helper Functions
+# ----------------
+
+
+def initialize_extensions(app) -> None:
+    # Since the application instance is now created, pass it to each Flask
+    # extension instance to bind it to the Flask application instance (app)
+    # db.init_app(app)
+    csrf_protection.init_app(app)
+
+    # Flask-Login configuration
+    # from project.models import User
+
+    # @login.user_loader
+    # def load_user(user_id):
+    #     return User.query.filter(User.id == int(user_id)).first()
+
+
+def register_blueprints(app) -> None:
+    # Since the application instance is now created, register each Blueprint
+    # with the Flask application instance (app)
+    from labconnect.main import main_blueprint
+    from labconnect.errors import error_blueprint
+
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(error_blueprint)
