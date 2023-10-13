063f280d43ae2d11666d0737f1e54d7144d6b0e9
Mrunal Athaley <112527910+athalm@users.noreply.github.com>
2023-10-13 15:18:12 -0400

    Created the PostOpportunity form
    Created the form for the professor to post details regarding research opportunities.

--- labconnect/forms.py
+++ labconnect/forms.py
@@ -8,9 +8,13 @@ from wtforms import (
     StringField,
     SubmitField,
     TextAreaField,
+    SelectField,
+    DateField,
+    SelectMultipleField
 )
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from wtforms.widgets import TextArea
+from datetime import datetime
 
 """
 Forms
@@ -18,5 +22,16 @@ Forms
 
 
 class PostOpportunity(FlaskForm):
+    MAJORS = []
+    COURSES = []
+    name = StringField('Job Title', validators=[DataRequired(), Length(max=150, message='Job Title must be 150 characters or less')])
+    description = TextAreaField('Job Description', widget=TextArea(), validators=[DataRequired(), Length(max=1500, message='Job Description must be 1500 characters or less')])
+    compensation = SelectField('Mode of Compensation', choices=['Pay','Credit','Both'], validators=[DataRequired()])
+    application_due = DateField('Application Due Date', format='%m/%d/%Y', validators=[DataRequired()])
+    recommended_class_years = SelectMultipleField('Recommended Class Years', choices=['Freshman','Sophomore','Junior','Senior','Grad Student'])
+    recommended_experiences = TextAreaField('Recommended Experiences', widget=TextArea(), validators=[Length(max=1500, message='Recommended experiences must be 1500 characters or less')])
+    recommended_majors = SelectMultipleField('Recommended Majors', choices=MAJORS)
+    recommended_courses = SelectMultipleField('Recommended Courses', choices=COURSES)
     email = StringField("Email", validators=[DataRequired(), Email(), Length(max=120)])
+    #active_semesters
     submit = SubmitField("Submit")
