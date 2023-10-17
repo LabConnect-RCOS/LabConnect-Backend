2b29700aed97b150d6881d82aad4d55e14724835
Rafael Cenzano <savagecoder77@gmail.com>
2023-10-17 17:32:34 -0400

    add new routes

--- labconnect/main/routes.py
+++ labconnect/main/routes.py
@@ -13,6 +13,21 @@ def positions():
     return render_template("positions.html")
 
 
+@main_blueprint.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@main_blueprint.route("/department")
+def department():
+    return render_template("department.html")
+
+
+@main_blueprint.route("/discover")
+def discover():
+    return render_template("discover.html")
+
+
 @main_blueprint.route("/login")
 def login():
     return "Login page"
