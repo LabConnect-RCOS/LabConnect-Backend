2e307ed2b3b8538aba44febe5923041bd00d1ff9
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2023-11-19 19:47:44 -0500

    Reformatted files in CSV_and_parsing with Black

--- docs/CSV_and_parsing/CSV_pythonscript.py
+++ docs/CSV_and_parsing/CSV_pythonscript.py
@@ -1,21 +1,21 @@
 import csv
 
-csv_file_name = 'Documents\GitHub\LabConnect\docs\CSV_and_parsing\CSVtest.csv'
+csv_file_name = "Documents\GitHub\LabConnect\docs\CSV_and_parsing\CSVtest.csv"
 
 
 data = []  # List to store rows of data
 
 try:
     # Storing the data
-    with open(csv_file_name, mode='r', newline='') as file:
+    with open(csv_file_name, mode="r", newline="") as file:
         reader = csv.reader(file)
         headers = next(reader)  # Read the first row as headers
 
         for row in reader:
             data.append(row)
-            
+
     # Printing the data
-    with open(csv_file_name, mode='r', newline='') as file:
+    with open(csv_file_name, mode="r", newline="") as file:
         reader = csv.reader(file)
         data = list(reader)
         headers = data[0]
@@ -25,15 +25,19 @@ try:
         column_widths = [max(len(cell) for cell in col) for col in zip(*data)]
 
         # Print headers
-        header_line = ' | '.join(f'{header:^{width}}' for header, width in zip(headers, column_widths))
-        separator = '-' * len(header_line)
+        header_line = " | ".join(
+            f"{header:^{width}}" for header, width in zip(headers, column_widths)
+        )
+        separator = "-" * len(header_line)
         print(separator)
         print(header_line)
         print(separator)
 
         # Print data
         for row in rows:
-            row_line = ' | '.join(f'{cell:<{width}}' for cell, width in zip(row, column_widths))
+            row_line = " | ".join(
+                f"{cell:<{width}}" for cell, width in zip(row, column_widths)
+            )
             print(row_line)
 
         print(separator)
