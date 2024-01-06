fd1cbcedfacb739b097c0224ede0cf738f68cad3
Abid Talukder <atalukder30@stuy.edu>
2024-01-05 19:41:25 -0500

    Created icon button and search bar

--- react-app/src/opportunities/components/FiltersField.js
+++ react-app/src/opportunities/components/FiltersField.js
@@ -1,12 +1,38 @@
 import React from "react";
 import SmallTextButton from "./SmallTextButton";
+import SearchBar from "./SearchBar";
+import GroupedComponents from "../../shared/components/UIElements/GroupedComponents";
+import HorizontalIconButton from "./HorizontalIconButton";
+import { PiSlidersHorizontal } from "react-icons/pi";
+import { MdCancel } from "react-icons/md";
 
 const FiltersField = () => {
   return (
     <div>
       <hr />
-      <div className="px-3 overflow-x-scroll max-h-20 ">
-        <SmallTextButton>All Filters</SmallTextButton>
+      <div className="px-3 overflow-x-scroll max-h-20 flex gap-3">
+        <div className="flex gap-2 w-full">
+          <SearchBar />
+
+          <GroupedComponents gap={2}>
+            <HorizontalIconButton icon={<MdCancel />}>
+              Credit
+            </HorizontalIconButton>
+
+            <HorizontalIconButton icon={<MdCancel />}>
+              Fall
+            </HorizontalIconButton>
+
+            <HorizontalIconButton icon={<MdCancel />}>
+              Physics
+            </HorizontalIconButton>
+          </GroupedComponents>
+        </div>
+
+        <SmallTextButton className="justify-end w-full" special={true}>
+          <PiSlidersHorizontal />
+          All Filters
+        </SmallTextButton>
       </div>
       <hr />
     </div>
---
+++ react-app/src/opportunities/components/HorizontalIconButton.js
@@ -0,0 +1,18 @@
+import React, { Children } from "react";
+
+const HorizontalIconButton = ({ children, onClick, icon, special }) => {
+  return (
+    <div
+      className={`${special && "font-medium"} ${
+        !special && "font-sm"
+      } flex gap-1 align-items-center p-2 px-2.5 rounded-3xl border`}
+    >
+      <button className="hover:text-red-600" onClick={onClick}>
+        {icon}
+      </button>
+      {children}
+    </div>
+  );
+};
+
+export default HorizontalIconButton;
