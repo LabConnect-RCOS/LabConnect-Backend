d3d7abde5fd53a60a9de16c466f14cbbcf57f427
Abid Talukder <atalukder30@stuy.edu>
2024-01-06 10:17:15 -0500

    Created job post component that holds small cards with job titles. Component is clickable and highlights to purple color if active

---
+++ react-app/src/opportunities/components/JobPost.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const JobPost = ({ title, professor, id, location, season, year, onClick, active }) => {
+  return (
+    <div className="p-2  min-h-36 flex gap-3 py-2">
+      <div className={`${active && "border-l-2 border-l-purple-600"}`} />
+      <div onClick={()=>{onClick(id)}} className="border-b border-b-gray-300 hover:bg-gray-100 p-1 rounded hover:cursor-pointer">
+        <h4 className="font-black">{title}</h4>
+        <div className="">
+          <h5 className="text-sm">{professor}</h5>
+          <h5 className="text-sm">
+            {location} · {season} {year}
+          </h5>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default JobPost;
--- react-app/src/shared/components/UIElements/Backdrop.js
+++ react-app/src/shared/components/UIElements/Backdrop.js
@@ -1,14 +1,11 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-
-const Backdrop = () => {
-    return ReactDOM.createPortal(
-      <div className="backdrop">
-        {/* Add content or styling as needed here */}
-      </div>,
-      document.getElementById("backdrop-hook")
-    );
-  }
+const Backdrop = ({ onClick }) => {
+  return ReactDOM.createPortal(
+    <div onClick={onClick} className="backdrop" />,
+    document.getElementById("backdrop-hook")
+  );
+};
 
 export default Backdrop;
