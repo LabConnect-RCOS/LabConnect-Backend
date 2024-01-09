525e6f037c30d8f66ba1325042c59b3e74fb2141
Abid Talukder <atalukder30@stuy.edu>
2024-01-09 12:34:14 -0500

    Created staff page and departments page that allows users to search by the different departments or research centers that rpi has

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Jobs from "./opportunities/pages/Jobs";
 import Browse from "./staff/pages/Browse";
 import Profile from "./staff/pages/Profile";
+import Center from "./staff/pages/Center";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/jobs" element={<Jobs />} />
           <Route path="/staff" element={<Browse />} />
+          <Route path="/center/:centerName" element={<Center />} />
           <Route path="/staff/:staffId" element={<Profile />} />
 
           <Route path="/*" element={<PageNotFound />} />
