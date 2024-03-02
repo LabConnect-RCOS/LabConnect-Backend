7dd5581caf0926a13f93d9f1b6a4bf5e86686f82
Mrunal Athaley <112527910+athalm@users.noreply.github.com>
2024-03-01 21:53:46 -0500

    added some filter options to the form

--- react-app/src/opportunities/components/FilterModal.js
+++ react-app/src/opportunities/components/FilterModal.js
@@ -18,7 +18,60 @@ const FilterModal = ({ open, handleClose }) => {
           {/* Modal content (filter options) */}
           <h2 id="transition-modal-title">Filter Options</h2>
           <div id="transition-modal-description">
-            
+          <h3>Major</h3>
+            <label>
+              <input type="checkbox" /> Computer Science
+            </label>
+            <label>
+              <input type="checkbox" /> ITWS
+            </label>
+            <label>
+              <input type="checkbox" /> Mechanical Engineering
+            </label>
+
+            <h3>Pay</h3>
+            <label>
+              <input type="checkbox" /> Paid
+            </label>
+            <label>
+              <input type="checkbox" /> Credit
+            </label>
+            <label>
+              <input type="checkbox" /> Pay + Credit
+            </label>
+
+            <h3>Onsite/remote</h3>
+            <label>
+              <input type="checkbox" /> In person
+            </label>
+            <label>
+              <input type="checkbox" /> Virtual
+            </label>
+
+            <h3>Qualifications</h3>
+            <label>
+              <input type="checkbox" /> Major
+            </label>
+            <label>
+              <input type="checkbox" /> GPA
+            </label>
+            <label>
+              <input type="checkbox" /> School Year
+            </label>
+            <label>
+              <input type="checkbox" /> Graduation Date
+            </label>
+
+            <h3>Work Authorization</h3>
+            <label>
+              <input type="checkbox" /> Jobs that do not require US work authorization
+            </label>
+            <label>
+              <input type="checkbox" /> Jobs that are eligible for US visa sponsorship
+            </label>
+            <label>
+              <input type="checkbox" /> Jobs that are open to candidates with Curricular Practical Training (CPT) and/or Optional Practical Training (OPT)
+            </label>
           </div>
         </div>
       </div>
