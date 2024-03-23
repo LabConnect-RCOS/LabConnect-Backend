82e3028683013df3c65f2aba35a9e0103d9ad336
Rafael Cenzano <savagecoder77@gmail.com>
2024-03-22 21:56:44 -0400

    remove not working error handler

--- labconnect/errors/routes.py
+++ labconnect/errors/routes.py
@@ -9,12 +9,6 @@ def handle_404(e) -> Response:
     return make_response({"error": "404 not found"}, 404)
 
 
-@error_blueprint.app_errorhandler(400)
-def handle_400(e) -> Response:
-    # 400 error route
-    return make_response({"error", "bad user request"}, 400)
-
-
 @error_blueprint.app_errorhandler(500)
 def handle_500(e) -> Response:
     # 500 error route
