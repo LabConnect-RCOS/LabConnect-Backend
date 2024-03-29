1c79c1579e514f9fe205af3c5c7a9b728e6fc62e
WILL BROADWELL <broadw@rpi.edu>
2024-03-29 19:19:14 -0400

    modified:   react-app/src/shared/pages/Home.js

--- react-app/src/shared/pages/Home.js
+++ react-app/src/shared/pages/Home.js
@@ -17,41 +17,52 @@ const Home = ({signOut, signIn}) => {
   }, []);
   
   return (
-      <section className="flex flex-col h-screen justify-between">
+    <section className="flex flex-col h-screen justify-between">
       <section className="text-center justify-center font-sans">
         <br/>
-        <img src="LabConnect_Logo.png" alt="Logo" className="flex justify-center"></img>
+        <img src="LabConnect_Logo.png" alt="Logo" className="flex"></img>
         <br/>
 
         <p className="text-xl">
           Welcome to LabConnect!
         </p>
         <p className="text-base">
-          If you are a student looking to search for research opportunities, go to the <Link to="/jobs" className="no-underline"><b>Jobs</b></Link> tab to start searching.<br/>
-          If you are a teacher, <Link className="no-underline"><b>Sign In</b></Link> and then go to <Link to="/createPost" className="no-underline"><b>Create</b></Link> to start posting opportunities
-          or <Link to="/profile" className="no-underline"><b>Profile</b></Link> to view and edit your current posts.
+          If you are a student, go to the <Link to="/jobs" className="no-underline"><b>Jobs</b></Link> tab to view currently available research opportunities.<br/>
+          If you are a professor or staff member, <Link className="no-underline"><b>Sign In</b></Link> and then go to <Link to="/createPost" className="no-underline"><b>Create</b></Link> to start posting <br/>
+          opportunities or <Link to="/profile" className="no-underline"><b>Profile</b></Link> to view and edit your current posts.
         </p>
       </section>
-      <section>
-        <p className="text-center text-xl justify-center font-serif">
-          An <Link to="https://new.rcos.io" className="no-underline text-red-400">RCOS</Link> Project
+      <section className="border-2 rounded p-2">
+        <p className="text-center text-lg justify-center">
+          Made by <Link to="https://new.rcos.io" className="no-underline text-red-400">RCOS</Link>
         </p>
-        <div className="flex flex-row justify-between">
-          <img src="misc/LabConnect_Logo.png" alt="Logo" className="w-60 h-60"></img>
-          <div>
-            <p className="text-center text-lg">
-              Contact Us:
+        <div className="flex flex-row justify-between ">
+          <img src="misc/LabConnect_Logo.png" alt="Logo" className="px-5 w-60 h-60"></img>
+          <div className="h-60 w-40">
+            <p className="text-center">
+              <b>Contact Us</b>
               <p className="text-base">
-                dummyemail@gmail.com<br/>other info
+                dummyemail@gmail.com<br/>other info(github/disc)
               </p>
             </p>
           </div>
-          <p className="w-60 h-60 text-lg">
-            Contributors:
-            <p className="text-sm">
-              X<br/>X<br/>X<br/>X<br/>X
+          <div className="h-60 w-40 text-base">
+            <b>Student Pages</b>
+            <p>
+              <Link to="/jobs" className="no-underline text-neutral-600 hover:text-neutral-950">Jobs</Link><br/>
+              <Link to="/staff" className="no-underline text-neutral-600 hover:text-neutral-950">Staff</Link><br/>
+              <Link to="/" className="no-underline text-neutral-600 hover:text-neutral-950">Sign In</Link><br/>
             </p>
-          </p>
+          </div>
+          <div className="h-60 w-60 text-base">
+            <b>Staff Pages</b>
+            <p>
+              <Link to="/profile" className="no-underline text-neutral-600 hover:text-neutral-950">Profile</Link><br/>
+              <Link to="/createPost" className="no-underline text-neutral-600 hover:text-neutral-950">Create</Link><br/>
+              <Link to="/jobs" className="no-underline text-neutral-600 hover:text-neutral-950">Jobs</Link><br/>
+              <Link to="/" className="no-underline text-neutral-600 hover:text-neutral-950">Sign In</Link><br/>
+            </p>
+          </div>
         </div>
       </section>
     </section>
