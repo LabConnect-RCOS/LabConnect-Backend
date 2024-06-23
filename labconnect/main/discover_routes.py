67fc8880b623ea48e2e17fec8b9c65f959600820
Rafael Cenzano <savagecoder77@gmail.com>
2024-06-23 10:13:38 -0500

    Complete an initial version of discover route

--- labconnect/main/discover_routes.py
+++ labconnect/main/discover_routes.py
@@ -37,54 +37,35 @@ from . import main_blueprint
 
 @main_blueprint.get("/discover")
 def discover():
-    query = (
-        db.select(
-            Opportunities,
-        )
-        .where(
-            Majors.code == "CSCI"
-        )  # in future: major_code: intake someone's department/major
-        # .where(
-        #     ClassYears.class_year == "2024"
-        # )  # match school year of student to oppoortunity
-        .where(
-            Opportunities.active == True
-        )  # ensure opportunity is active & not past the deadline
-        .join(RecommendsMajors, Opportunities.id == RecommendsMajors.opportunity_id)
-        .join(Majors, RecommendsMajors.major_code == Majors.code)
-        # .join(Opportunities, RecommendsMajors.opportunity_id == Opportunities.id)
-        .join(RecommendsClassYears, Opportunities.id == RecommendsClassYears.class_year)
-        .join(ClassYears, RecommendsClassYears.class_year == ClassYears.class_year)
-        .limit(5)  # limit how many things are returned from the query (4 items)
-        .order_by(Opportunities.last_updated.desc())
-    )
-
-    print(query)
-    data = db.session.execute(query)
-    print("HERE")
-    print(data)
-    result = [opportunity.to_dict() for opportunity in data]
+    result = discover_data(get_jwt_identity(), 5)
     return result
 
+def discover_data(jwt_identity, limit):
+    data = []
+    if jwt_identity is not None:
+        user = db.select(User).where(User.email == jwt_identity).first()
+        query = (
+            db.select(
+                Opportunities,
+            )
+            .where(
+                Opportunities.active == True
+            )
+            .join(RecommendsClassYears, Opportunities.id == RecommendsClassYears.class_year)
+            .join(ClassYears, RecommendsClassYears.class_year == ClassYears.class_year)
+            .where(
+                ClassYears.class_year == user.class_year
+            )
+            .limit(limit)
+            .order_by(Opportunities.last_updated.desc())
+        )
 
-# example json below
-#     "data": [
-#         {
-#             "title": "Nelson",
-#             "major": "CS",
-#             # "experience": "x",
-#             # "description": "d",
-#             "attributes": ["Competitive Pay", "Four Credits", "Three Credits"],
-#             "credits": 4,
-#             "pay": 9000.0,
-#         },
-#         {
-#             "title": "Name",
-#             "major": "Major",
-#             # "experience": "XP",
-#             # "description": "Hi",
-#             "attributes": ["Competitive Pay", "Four Credits", "Three Credits"],
-#             "credits": 3,
-#             "pay": 123,
-#         },
-#     ]
+        majors = [Majors.code == user_major.major_code for user_major in db.select(UserMajors).where(UserMajors.user_id == user.id).scalars()]
+        query = query.join(Majors, RecommendsMajors.major_code == Majors.code)
+        query = query.where(db.or_(*majors))
+
+        data = db.session.execute(query).scalars()
+    if jwt_identity is None or data is None:
+        data = db.session.execute(db.select(Opportunities).where(Opportunities.active == True).limit(limit).order_by(Opportunities.last_updated.desc()))
+    result = [opportunity.to_dict() for opportunity in data]
+    return result
