796df2939ea6e4c09adf29bf886d1f3ce595866c
youssefr26 <ryoussef5087@gmail.com>
2024-09-24 16:48:37 -0400

    Added migration

--- labconnect/__init__.py
+++ labconnect/__init__.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta, timezone
 # Import Flask modules
 from flask import Flask
 from flask_cors import CORS
+
 from flask_jwt_extended import (
     JWTManager,
     create_access_token,
@@ -12,11 +13,12 @@ from flask_jwt_extended import (
     get_jwt_identity,
 )
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 from labconnect.helpers import OrJSONProvider
 
 # Create Database object
 db = SQLAlchemy()
+migrate = Migrate()
 jwt = JWTManager()
 
 
@@ -42,6 +44,7 @@ def initialize_extensions(app) -> None:
     # Since the application instance is now created, pass it to each Flask
     # extension instance to bind it to the Flask application instance (app)
     db.init_app(app)
+    migrate.init_app(app, db)
     jwt.init_app(app)
     app.json = OrJSONProvider(app)
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # Eventually add alembic migrations here
-
+flask db upgrade
 gunicorn run:app -w 6 --preload --max-requests-jitter 300 --bind 0.0.0.0:8000
