8e58ff1bce2d18c7960d67beb6dbc5a30749f86b
Mrunal Athaley <112527910+athalm@users.noreply.github.com>
2024-10-18 14:02:51 -0400

    update test_opportunities_filtering.py
    test parameterization

--- tests/test_opportunities_filtering.py
+++ tests/test_opportunities_filtering.py
@@ -2,284 +2,85 @@
 Test opportunity filtering routes
 """
 
+import pytest
 from flask import json
 from flask.testing import FlaskClient
 
-
-def test_opportunity_filter_pay(test_client: FlaskClient) -> None:
+@pytest.mark.parametrize(
+    "filters, expected_opportunities",
+    [
+        (
+            [{"field": "pay", "value": {"min": 14.9, "max": 21}}],
+            ["Automated Cooling System"]
+        ),
+        (
+            [{"field": "departments", "value": ["Material Science"]}],
+            ["Checking out cubes"]
+        ),
+        (
+            [{"field": "departments", "value": ["Computer Science", "Material Science"]}],
+            ["Iphone 15 durability test", "Checking out cubes", "Automated Cooling System"]
+        ),
+        (
+            [{"field": "majors", "value": ["BIOL"]}],
+            ["Iphone 15 durability test", "Checking out cubes", "Automated Cooling System"]
+        ),
+        (
+            [{"field": "majors", "value": ["CSCI", "BIOL"]}],
+            ["Iphone 15 durability test", "Checking out cubes", "Automated Cooling System"]
+        ),
+        (
+            [{"field": "credits", "value": [1]}],
+            ["Iphone 15 durability test", "Checking out cubes"]
+        ),
+        (
+            [{"field": "credits", "value": [2, 4]}],
+            ["Iphone 15 durability test", "Checking out cubes", "Automated Cooling System", "Test the water"]
+        ),
+        (
+            [{"field": "class_year", "value": [2025]}],
+            ["Iphone 15 durability test"]
+        ),
+        (
+            [{"field": "class_year", "value": [2025, 2027]}],
+            ["Iphone 15 durability test", "Automated Cooling System"]
+        ),
+        (
+            [{"field": "location", "value": "Remote"}],
+            ["Automated Cooling System"]
+        ),
+        (
+            [{"field": "location", "value": "In-Person"}],
+            ["Iphone 15 durability test", "Checking out cubes", "Test the water"]
+        ),
+        (
+            [
+                {"field": "location", "value": "In-Person"},
+                {"field": "departments", "value": ["Computer Science"]}
+            ],
+            ["Iphone 15 durability test"]
+        ),
+        (
+            [
+                {"field": "credits", "value": [2, 4]},
+                {"field": "departments", "value": ["Computer Science"]}
+            ],
+            ["Iphone 15 durability test", "Automated Cooling System"]
+        ),
+    ],
+)
+def test_opportunity_filter(test_client: FlaskClient, filters, expected_opportunities) -> None:
     """
     GIVEN a Flask application configured for testing
     WHEN the '/opportunity/filter' page is requested (GET)
     THEN check that the response is valid
     """
-
-    json_data = {"filters": [{"field": "pay", "value": {"min": 14.9, "max": 21}}]}
+    json_data = {"filters": filters}
     response = test_client.get("/opportunity/filter", json=json_data)
 
     assert response.status_code == 200
 
     json_data = json.loads(response.data)
-
-    assert "Automated Cooling System" == json_data[0]["name"]
-
-
-def test_opportunity_filter_department(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {"filters": [{"field": "departments", "value": ["Material Science"]}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    assert "Checking out cubes" == json_data[0]["name"]
-
-
-def test_opportunity_filter_departments(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {
-        "filters": [
-            {"field": "departments", "value": ["Computer Science", "Material Science"]}
-        ]
-    }
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = (
-        "Iphone 15 durability test",
-        "Checking out cubes",
-        "Automated Cooling System",
-    )
 
     for data in json_data:
-        assert data["name"] in opportunities
-
-
-def test_opportunity_filter_major(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {"filters": [{"field": "majors", "value": ["BIOL"]}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = (
-        "Iphone 15 durability test",
-        "Checking out cubes",
-        "Automated Cooling System",
-    )
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-
-def test_opportunity_filter_majors(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {"filters": [{"field": "majors", "value": ["CSCI", "BIOL"]}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = (
-        "Iphone 15 durability test",
-        "Checking out cubes",
-        "Automated Cooling System",
-    )
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-
-def test_opportunity_filter_credits(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {"filters": [{"field": "credits", "value": [1]}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = (
-        "Iphone 15 durability test",
-        "Checking out cubes",
-    )
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-    json_data = {"filters": [{"field": "credits", "value": [2, 4]}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = (
-        "Iphone 15 durability test",
-        "Checking out cubes",
-        "Automated Cooling System",
-        "Test the water",
-    )
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-
-def test_opportunity_filter_class_years(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {"filters": [{"field": "class_year", "value": [2025]}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = ("Iphone 15 durability test",)
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-    json_data = {"filters": [{"field": "class_year", "value": [2025, 2027]}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = (
-        "Iphone 15 durability test",
-        "Automated Cooling System",
-    )
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-
-def test_opportunity_filter_location_remote(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {"filters": [{"field": "location", "value": "Remote"}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    assert "Automated Cooling System" == json_data[0]["name"]
-
-
-def test_opportunity_filter_location_in_person(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {"filters": [{"field": "location", "value": "In-Person"}]}
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = (
-        "Iphone 15 durability test",
-        "Checking out cubes",
-        "Test the water",
-    )
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-
-def test_opportunity_filter_location_departments(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {
-        "filters": [
-            {"field": "location", "value": "In-Person"},
-            {"field": "departments", "value": ["Computer Science"]},
-        ]
-    }
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = ("Iphone 15 durability test",)
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-
-def test_opportunity_filter_credits_departments(test_client: FlaskClient) -> None:
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/opportunity/filter' page is requested (GET)
-    THEN check that the response is valid
-    """
-
-    json_data = {
-        "filters": [
-            {"field": "credits", "value": [2, 4]},
-            {"field": "departments", "value": ["Computer Science"]},
-        ]
-    }
-    response = test_client.get("/opportunity/filter", json=json_data)
-
-    assert response.status_code == 200
-
-    json_data = json.loads(response.data)
-
-    opportunities = ("Iphone 15 durability test", "Automated Cooling System")
-
-    for data in json_data:
-        assert data["name"] in opportunities
-
-
-# TODO: Add test for no fields
+        assert data["name"] in expected_opportunities
