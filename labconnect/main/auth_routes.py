adf5dd97760bdbeede534be695dcd9d6f48f2c80
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-29 16:51:12 -0400

    Improved auth system that encrypts token in a response

--- labconnect/main/auth_routes.py
+++ labconnect/main/auth_routes.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from uuid import uuid4
 
 from flask import current_app, make_response, redirect, request
 from flask_jwt_extended import create_access_token
@@ -10,16 +11,49 @@ from labconnect.models import User
 
 from . import main_blueprint
 
+temp_codes = {}
+
+
+def generate_temporary_code(user_email: str) -> str:
+    # Generate a unique temporary code
+    code = str(uuid4())
+    expires_at = datetime.now() + timedelta(seconds=60)  # expires in 5 seconds
+    temp_codes[code] = {"email": user_email, "expires_at": expires_at}
+    return code
+
+
+def validate_code_and_get_user_email(code: str) -> str | None:
+    token_data = temp_codes.get(code, {})
+    if not token_data:
+        return None
+
+    user_email = token_data.get("email", None)
+    expire = token_data.get("expires_at", None)
+
+    if user_email and expire and expire > datetime.now():
+        # If found, delete the code to prevent reuse
+        del temp_codes[code]
+        return user_email
+    elif expire:
+        # If the code has expired, delete it
+        del temp_codes[code]
+
+    return None
+
 
 @main_blueprint.get("/login")
 def saml_login():
 
-    if current_app.config["TESTING"]:
+    # In testing skip RPI login purely for local development
+    if (
+        current_app.config["TESTING"]
+        and current_app.config["Frontend_URL"] == "http://localhost:3000"
+    ):
         # Generate JWT
-        token = create_access_token(identity=["test", datetime.now()])
+        code = generate_temporary_code("test@rpi.edu")
 
         # Send the JWT to the frontend
-        return redirect(f"{current_app.config['FRONTEND_URL']}/token/?token={token}")
+        return redirect(f"{current_app.config['FRONTEND_URL']}/callback/?code={code}")
 
     # Initialize SAML auth request
     req = prepare_flask_request(request)
@@ -37,7 +71,7 @@ def saml_callback():
 
     if not errors:
         user_info = auth.get_attributes()
-        user_id = auth.get_nameid()
+        # user_id = auth.get_nameid()
 
         data = db.session.execute(db.select(User).where(User.email == "email")).scalar()
 
@@ -57,12 +91,30 @@ def saml_callback():
             db.session.commit()
 
         # Generate JWT
-        token = create_access_token(identity=[user_id, datetime.now()])
+        # token = create_access_token(identity=[user_id, datetime.now()])
+        code = generate_temporary_code(user_info["email"][0])
 
         # Send the JWT to the frontend
-        return redirect(f"{current_app.config['FRONTEND_URL']}/token/?token={token}")
+        return redirect(f"{current_app.config['FRONTEND_URL']}/callback/?code={code}")
 
     return {"errors": errors}, 500
+
+
+@main_blueprint.post("/token")
+def token():
+    if request.json is None or request.json.get("code", None) is None:
+        return {"msg": "Missing JSON body in request"}, 400
+    # Validate the temporary code
+    code = request.json["code"]
+    if code is None:
+        return {"msg": "Missing code in request"}, 400
+    user_email = validate_code_and_get_user_email(code)
+
+    if user_email is None:
+        return {"msg": "Invalid code"}, 400
+
+    token = create_access_token(identity=[user_email, datetime.now()])
+    return {"token": token}
 
 
 @main_blueprint.get("/metadata/")
