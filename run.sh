1526c8df8e9d9aa5351181ba6d96bbf0512a05a2
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-01-31 16:33:02 -0500

    Add back commands to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 set -e  # Fail on any error
 
+python db_init.py start || exit 1
+
 # Migrate DB
-flask db upgrade
+flask db upgrade || exit 1
 
 # Run the app
 gunicorn app:app -w 6 --preload --max-requests-jitter 300 --bind 0.0.0.0:9000 || exit 1
