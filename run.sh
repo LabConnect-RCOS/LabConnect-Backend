d5997d86080f0483b05a37956d6d659a645b56d7
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-01-31 15:33:01 -0500

    fix run script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e  # Fail on any error
 python db_init.py start || exit 1
 
 # Migrate DB
-flask db upgrade || exit 1
+flask db upgrade
 
 # Run the app
 gunicorn app:app -w 6 --preload --max-requests-jitter 300 --bind 0.0.0.0:9000 || exit 1
